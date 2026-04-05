e42e98b986e799795b87c751167b5fa9a44a94e9
Phillipa <pbennetteghan.work@gmail.com>
2026-04-05 13:00:21 +0000

    added jwt setup

--- app/routes/photos.py
+++ app/routes/photos.py
@@ -2,23 +2,24 @@ import os
 
 import cloudinary
 import cloudinary.api
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from app.auth.jwt import get_current_user
+from app.db.models import User
+
+cloudinary.config(
+    cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
+    api_key=os.environ["CLOUDINARY_API_KEY"],
+    api_secret=os.environ["CLOUDINARY_API_SECRET"],
+)
 
 router = APIRouter()
 
 @router.get("/api/photos")
-def get_photos():
-
-    cloudinary.config(
-        cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
-        api_key=os.environ["CLOUDINARY_API_KEY"],
-        api_secret=os.environ["CLOUDINARY_API_SECRET"],
-    )
-
+async def get_photos(current_user: User = Depends(get_current_user)):
     result = cloudinary.api.resources(
         type="upload",
-        prefix="publr/",
+        prefix=f"publr/{current_user.id}/",
         max_results=100,
     )
     urls = [r["secure_url"] for r in result["resources"]]
